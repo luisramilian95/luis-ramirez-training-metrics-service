983c2c9d81503cef5d15bde8d9f6ea7cbab7de6d
Luis Ramirez <leramirez@xumak.com>
2022-07-26 10:37:06 -0600

    Init project Training Metrics Service

---
+++ src/main/java/com/xumak/metrics/TrainingMetricsServiceApplication.java
@@ -0,0 +1,14 @@
+package com.xumak.metrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingMetricsServiceApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingMetricsServiceApplication.class, args);
+	}
+
+}
