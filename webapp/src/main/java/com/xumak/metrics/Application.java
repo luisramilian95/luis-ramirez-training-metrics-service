ae266a46ecd41b5cd5d0709b8ce277bc0997c7a7
Luis Ramirez <leramirez@xumak.com>
2022-07-26 14:11:36 -0600

    ADD: Modular Project

--- webapp/src/main/java/com/xumak/metrics/Application.java
+++ webapp/src/main/java/com/xumak/metrics/Application.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TrainingMetricsServiceApplication {
-
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TrainingMetricsServiceApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
